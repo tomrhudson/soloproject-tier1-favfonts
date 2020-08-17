ad3c880f5ed7ff81871925e91d73b9aa3e6ac675
Tom Hudson <trhudson@gmail.com>
2020-08-17 11:33:15 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+//jshint esversion:6
 
+function hamburgerNavBarIcon() {
+
+  // Top Navigation Bar Function
+  var x = document.getElementById("topNavBar");
+  if (x.className === "nav-bar") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav-bar";
+  };
+}
