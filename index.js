0328b361c46d23e0a207c3bb5139d936d0f2af3d
Tom Hudson <trhudson@gmail.com>
2020-08-19 11:29:37 -0400

    Created JavaScript Functions
    Created the following JS functions:
    1. changeSampleText
    2. resetAll
    3. changeFontSize
    
    Updated events on index.html for:
    1. Sample Text input box
    2. reset button
    3. drop down font size selector

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 //jshint esversion:6
 
+const originalSampleText = "Almost before we knew it, we had left the ground.";
+
 function hamburgerNavBarIcon() {
 
   // Top Navigation Bar Function
   var x = document.getElementById("topNavBar");
+
   if (x.className === "nav-bar") {
     x.className += " responsive";
   } else {
@@ -20,13 +23,49 @@ function changeFontCardLayout() {
 }
 
 function changeFontSize() {
+  var fontCards = document.querySelectorAll(".font-card-sample > p");
+  var selectedFontSize = document.getElementById("font-size").value;
+  var currentCounter = 0;
 
+  // console.log("FUNCTION: change font size");
+  // console.log("# of font cards: " + fontCards.length);
+  // console.log("selected font size: " + selectedFontSize);
+
+  for (currentCounter; currentCounter < fontCards.length; currentCounter++) {
+    // console.log("loop count: " + currentCounter);
+    // console.log("current font size: " + fontCards[counter].style.fontSize);
+    fontCards[currentCounter].style.fontSize = selectedFontSize;
+  };
 }
 
-function reset() {
+function changeSampleText(event) {
+  var currentCounter = 0;
+  var fontCards = document.querySelectorAll(".font-card-sample > p");
+  var userSampleText = document.getElementById("sample-text").value.trim();
 
+  // if sample text is empty, then reset the value back to the originalSampleText
+
+  if (userSampleText !== "") {
+    for (currentCounter; currentCounter < fontCards.length; currentCounter++) {
+      fontCards[currentCounter].innerHTML = userSampleText;
+    };
+  } else {
+    userSampleText = originalSampleText;
+    for (currentCounter; currentCounter < fontCards.length; currentCounter++) {
+      fontCards[currentCounter].innerHTML = userSampleText;
+    };
+  };
 }
 
-function changeSampleText() {
+function resetAll() {
+  // reset values back to page launch values
+  document.getElementById("font-size").value = "20px";
+  document.getElementById("sample-text").value = originalSampleText;
+  document.getElementById("search-fonts").value = "Search fonts";
 
+  //call functions that reset font cards based on reset values
+  changeFontSize();
+  changeColor();
+  changeFontCardLayout();
+  changeSampleText();
 }
